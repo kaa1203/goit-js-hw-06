2db91069e7871b18eb2616ae56afd79c6d4c6124
kaa1203 <144120967+kaa1203@users.noreply.github.com>
2024-02-01 01:39:06 +0800

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -20,10 +20,16 @@ body.appendChild(container);
 
 create.addEventListener("click", () => {
   let index = boxNum.valueAsNumber;
+  let max = boxNum.getAttribute("max")
+  if (index > max) {
+    return alert(`Up to ${max} only`)
+  }
   for (let i = 1; i <= index; i++) {
     let div = document.createElement("div");
+    div.setAttribute("id", i);
     div.style.width = "30px";
     div.style.height = "30px";
+    
     div.style.backgroundColor = getRandomHexColor();
     container.appendChild(div);
   }
