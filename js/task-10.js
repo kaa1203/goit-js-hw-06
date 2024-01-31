2149c14f7e1a96d79fde1151770659e4a0ca365a
kaa1203 <144120967+kaa1203@users.noreply.github.com>
2024-02-01 02:05:09 +0800

    Finished task 10

--- js/task-10.js
+++ js/task-10.js
@@ -21,18 +21,21 @@ body.appendChild(container);
 let total = 0;
 create.addEventListener("click", () => {
   let index = boxNum.valueAsNumber;
-  let max = boxNum.getAttribute("max")
+  let max = boxNum.getAttribute("max");
+
   if (index > max) {
     return alert(`Up to ${max} only`)
   }
+
   for (let i = 1; i <= index; i++) {
     let div = document.createElement("div");
-    div.setAttribute("id", i);
-    div.style.width = `${30*i}px`;
-    div.style.height = "30px";
-    
+    let size = i * 10;
+    console.log(size);
+    div.style.width = `${30+ size}px`;
+    div.style.height = `${30+ size}px`;
     div.style.backgroundColor = getRandomHexColor();
     container.appendChild(div);
   }
 });
+
 destroy.addEventListener("click", () => container.innerHTML = "");
--- readme.md
+++ readme.md
@@ -1,3 +1,5 @@
 01/31/2024
 Finished task 1-9 <br>
-task 10 (95%);
+task 10 (95%); <br>
+02/01/2024
+Finished HW
